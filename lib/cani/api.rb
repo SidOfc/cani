e57456e130e0bfa538399e66c05eb16da1e61bf1
Sidney Liebrand <sidneyliebrand@gmail.com>
2018-07-08 19:12:34 +0200

    Add method to force download data and update completions

--- lib/cani/api.rb
+++ lib/cani/api.rb
@@ -7,33 +7,40 @@ require_relative 'api/feature'
 module Cani
   class Api
     def initialize
-      @upd  = false
-      @data = begin
-        data_file       = File.join config.directory, 'caniuse.json'
-        data_exists     = File.exist? data_file
-        data_up_to_date = Time.now.to_i - File.mtime(data_file).to_i < config.expire.to_i
+      @data = load_data
+    end
 
-        if data_exists && data_up_to_date
-          # data is available and up to date
-          JSON.parse File.read(data_file)
-        elsif raw
-          @upd = true
-          # data either doesn't exist or isn't up to date
-          # if we can fetch new data, attempt to update
-          FileUtils.mkdir_p File.dirname(data_file)
-          File.open(data_file, 'w') { |f| f << raw }
-          JSON.parse raw
-        elsif data_exists
-          # if we are unable fetch new data, fall back
-          # to existing data if it exists
-          JSON.parse File.read(data_file)
-        else
-          # no other option but fail since we have no data
-          # and no way of fetching the data to display
-          puts 'fatal: no data available for display'
-          exit 1
-        end
+    def load_data(fetch: false)
+      @upd = false
+      data_file       = File.join config.directory, 'caniuse.json'
+      data_exists     = File.exist? data_file
+      data_up_to_date = data_exists ? (Time.now.to_i - File.mtime(data_file).to_i < config.expire.to_i)
+                                    : false
+
+      if !fetch && data_exists && data_up_to_date
+        # data is available and up to date
+        JSON.parse File.read(data_file)
+      elsif (data = raw)
+        @upd = true
+        # data either doesn't exist or isn't up to date
+        # if we can fetch new data, attempt to update
+        FileUtils.mkdir_p File.dirname(data_file)
+        File.open(data_file, 'w') { |f| f << data }
+        JSON.parse data
+      elsif data_exists
+        # if we are unable fetch new data, fall back
+        # to existing data if it exists
+        JSON.parse File.read(data_file)
+      else
+        # no other option but fail since we have no data
+        # and no way of fetching the data to display
+        puts 'fatal: no data available for display'
+        exit 1
       end
+    end
+
+    def update!
+      @data = load_data fetch: true
     end
 
     def updated?
@@ -64,7 +71,7 @@ module Cani
     end
 
     def raw
-      @data ||= begin
+      begin
         Net::HTTP.get URI(config.source)
       rescue
         nil
