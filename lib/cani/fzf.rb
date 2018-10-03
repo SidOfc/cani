2ee39b74b8bf7aed5bfad46f2eadf8008dcf4c6c
Sidney Liebrand <sidneyliebrand@gmail.com>
2018-10-03 20:46:16 +0200

    Simplify executable detection code

--- lib/cani/fzf.rb
+++ lib/cani/fzf.rb
@@ -22,10 +22,7 @@ module Cani
     end
 
     def self.executable?
-      @exe ||= begin
-        `command -v fzf;`.chomp
-        $?.success?
-      end
+      @exe ||= !system('fzf --version').nil?
     end
 
     def self.feature_rows
